fdcc7a33f2158926aa6719fa7e4ea919d4cfa72a
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-08-05 00:50:51 -0400

    se añaden funcionalidades

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.navbar-nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
